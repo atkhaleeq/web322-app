637134813ac968748235dd80d2e2b11d447a9147
Ahmad Tamim Khaleeq <atkhaleeq@myseneca.ca>
2024-06-15 13:57:32 -0400

    Adding declaration and minor fixes

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+WEB322 – Assignment 02
+I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+No part of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
+
+Name: Tamim Khaleeq
+Student ID: 173107228
+Date: 2024-06-15
+Vercel Web App URL: https://vercel.com/new/atkhaleeqs-projects/success?developer-id=&external-id=&redirect-url=&branch=master&deploymentUrl=web322-i430787gs-atkhaleeqs-projects.vercel.app&projectName=web322-app&s=https%3A%2F%2Fgithub.com%2Fatkhaleeq%2Fweb322-app&gitOrgLimit=&hasTrialAvailable=1&totalProjects=1&slug=app-future&slug=en-US&slug=new&slug=atkhaleeqs-projects&slug=success 
+GitHub Repository URL:  atkhaleeq/web322-app
+
+********************************************************************************/ 
+
 const express = require('express');
 const path = require('path');
 const store = require('./store-service');
@@ -43,7 +56,7 @@ store.initialize()
             res.status(404).send('404 Error :(');
         });
 
-        // Startint the server
+        // Starting the server
         app.listen(PORT, () => {
             console.log(`Express listening on port ${PORT}`);
         });
